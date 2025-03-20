42aa7ccc407670fc45158b99eae129555e4c48da
seyfullahseren <seyfullah.seren@akademiata.edu.pl>
2025-03-20 13:38:23 +0000

    Add new file

---
+++ hello.py/test1on1.py
@@ -0,0 +1,11 @@
+my_string = "hello my name seyfullah "
+print(my_string)
+
+fruits= ["banana", "cherry","kıwı"]
+for fruit in fruits:
+    print(fruit)
+
+def add_numbers(a , b):
+    return a + b 
+result = add_numbers(10,15)
+print("sum:",result)
